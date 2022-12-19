b19bc745580a7dd99db3f5b643b6fbbf401565a5
Kamuran Sahin <kamuransahin.c11@gmail.com>
2022-12-20 00:55:44 +0300

    selenium day1

---
+++ JavaProgrammingReviews_EU10/src/week15_12_17_2022/is_a_has_a/Developer.java
@@ -0,0 +1,4 @@
+package week15_12_17_2022.is_a_has_a;
+        //Developer IS AN Employee
+public class Developer extends Employee{
+}
---
+++ JavaProgrammingReviews_EU10/src/week15_12_17_2022/is_a_has_a/ScrumTeam.java
@@ -0,0 +1,20 @@
+package week15_12_17_2022.is_a_has_a;
+
+public class ScrumTeam {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 21; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+
+
+
